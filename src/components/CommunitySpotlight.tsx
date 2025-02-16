b928e8fdf8bc22ed4f33d7f2bbe25fc1ba17d7b1
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2025-02-16 12:46:43 +0530

    ui

---
+++ src/components/CommunitySpotlight.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import React from "react";
+
+function CommunitySpotlight() {
+  return (
+    <div>
+      <div className="grid grid-cols-5">
+        {[1, 2, 3, 4].map((i) => (
+          <div key={i} className="p-2">
+            <div className="card rounded-none bg-base-100 shadow-xl">
+              <figure>
+                <img
+                  src="https://images.pexels.com/photos/217872/pexels-photo-217872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  MTA Member Completed Saddlesore 1000
+                </h2>
+              </div>
+            </div>
+          </div>
+        ))}
+        <Link
+          href={"/"}
+          className="card bg-base-100 shadow-xl flex justify-center items-center rounded-none"
+        >
+          <div className="text-xl font-semibold text-center">
+            View <br /> Community Spotlights
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default CommunitySpotlight;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import Spotlight from "@/components/Spotlight";
 import JoinMta from "@/components/JoinMta";
 import Sponsors from "@/components/Sponsors";
+import CommunitySpotlight from "@/components/CommunitySpotlight";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Navbar />
       <Spotlight />
       <JoinMta />
+      <CommunitySpotlight />
       <Sponsors />
       <Footer />
     </div>

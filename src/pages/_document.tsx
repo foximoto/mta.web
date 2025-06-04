19aabd12b92e910652108557754469a90f90ec9b
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2025-06-04 20:37:26 +0530

    tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="98045a62-9ba9-49b7-9ef4-4843d26f2956"
+        ></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
